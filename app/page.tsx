70814ffc5879dc60c7763eba6ec1564536b575bf
Mykyta Holda <kitagolda@gmail.com>
2023-12-16 15:49:23 +0200

    Apollo Client with multiple endpoints logic and type generation

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { GetCharactersDocument } from "@/gql/queries/getCharacters/getCharacters.rickAndMorty.generated";
+import { GetSpaceXShipsDocument } from "@/gql/queries/getShips/getShips.spaceX.generated";
+import { GetSpeciesDocument } from "@/gql/queries/getSpecies/getSpecies.swapi.generated";
+import { useQuery } from "@apollo/client";
 
 export default function Home() {
+  const { data } = useQuery(GetSpaceXShipsDocument, {
+    variables: { limit: 10, offset: 0 },
+  });
+
+  const { data: rickAndMortyCharacters } = useQuery(GetCharactersDocument, {
+    variables: { page: 0 },
+    context: {
+      apiName: "rickAndMorty",
+    },
+  });
+
+  const { data: swapiCharacters } = useQuery(GetSpeciesDocument);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <main>
+        <div style={{ display: "flex", gap: "16px" }}>
+          <div>
+            <h1>SpaceX</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+          <div>
+            <h1>Rick and Morty</h1>
+            <pre>{JSON.stringify(rickAndMortyCharacters, null, 2)}</pre>
+          </div>
+          <div>
+            <h1>SWAPI</h1>
+            <pre>{JSON.stringify(swapiCharacters, null, 2)}</pre>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
